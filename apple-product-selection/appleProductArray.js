41e93a71b84ec0349bb7b9519f4dff41cd5114d1
LucasLi <alanlucaslee@gmail.com>
2023-01-10 07:47:33 +0800

    提交

---
+++ apple-product-selection/appleProductArray.js
@@ -0,0 +1,77 @@
+const appleList = [
+    {
+        type: 'iPhone',
+        name: 'iPhone 14 Pro',
+        price: 34900,
+        mainImg: './img/iphone-14-pro-finish-select-202209-6-1inch.jfif',
+        colorList: [
+            { name: '銀色', color: '#F1F3F2', img: './img/iphone-14-pro-finish-select-202209-6-1inch-silver.jfif' },
+            { name: '金色', color: '#F4E8CE', img: './img/iphone-14-pro-finish-select-202209-6-1inch-gold.jfif' },
+            { name: '深紫色', color: '#696071', img: './img/iphone-14-pro-finish-select-202209-6-1inch-deeppurple.jfif' },
+            { name: '太空黑色', color: '#4F4E4C', img: './img/iphone-14-pro-finish-select-202209-6-1inch-spaceblack.jfif' },
+        ],
+        spec: [
+            {
+                name: '儲存裝置。選擇需要的儲存空間大小。',
+                specDetails: [
+                    {
+                        name: '128GB',
+                        fit: 0
+                    },
+                    {
+                        name: '256GB',
+                        fit: 3500
+                    },
+                    {
+                        name: '512GB',
+                        fit: 10500
+                    },
+                    {
+                        name: '1TB',
+                        fit: 17500
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        type: 'iPad',
+        name: 'iPad 10',
+        price: 14900,
+        mainImg: './img/ipad-10th-gen-finish-unselect-gallery-1-202212.jfif',
+        colorList: [
+            { name: '銀色', color: '#E3E4E5', img: './img/ipad-10th-gen-finish-select-202212-silver.jfif' },
+            { name: '黃色', color: '#F0D96D', img: './img/ipad-10th-gen-finish-select-202212-yellow.jfif' },
+            { name: '藍色', color: '#738AA8', img: './img/ipad-10th-gen-storage-select-202212-blue.jfif' },
+            { name: '紅色', color: '#DE6D7C', img: './img/ipad-10th-gen-finish-select-202212-pink.jfif' },
+        ],
+        spec: [
+            {
+                name: '儲存裝置',
+                specDetails: [
+                    {
+                        name: '64GB',
+                        fit: 0
+                    },
+                    {
+                        name: '256GB',
+                        fit: 5000
+                    },
+                ]
+            },
+            {
+                name: '連線能力',
+                specDetails: [
+                    {
+                        name: 'Wi-Fi',
+                        fit: 0
+                    },
+                    {
+                        name: 'Wi-Fi + 行動網路',
+                        fit: 5000
+                    },
+                ]
+            },
+        ]
+    },
+]
